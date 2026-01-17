a427461ab3fd5f1cfc231627b81d293aafa420ea
kh-thien <khacthien.brand@gmail.com>
2026-01-17 11:17:09 +0700

    Initial Commit

---
+++ scrume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scrume
+//
+//  Created by NEIHT on 17/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
