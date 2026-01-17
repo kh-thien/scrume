4745d1341c9b5ec5f9cb6dea3176015c8be01f1d
kh-thien <khacthien.brand@gmail.com>
2026-01-17 11:17:09 +0700

    init: initial commit

---
+++ scrume/scrumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumeApp.swift
+//  scrume
+//
+//  Created by NEIHT on 17/1/26.
+//
+
+import SwiftUI
+
+@main
+struct scrumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
